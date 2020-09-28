a4585a10fc3e562d63ee2dfaf7e7c21e2ca7bd70
navyd <dhjnavyd@gmail.com>
2020-09-29 01:34:07 +0800

    class path root class test

--- src/test/java/xyz/navyd/mbg/GeneratorTest.java
+++ src/test/java/xyz/navyd/mbg/GeneratorTest.java
@@ -3,6 +3,7 @@ package xyz.navyd.mbg;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -13,40 +14,49 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
-import test.dao.UserInfoMapper;
+import xyz.navyd.mbg.dao.BaseMapper;
+import xyz.navyd.mbg.entity.BaseEntity;
 
 
 public class GeneratorTest {
-  @BeforeAll
-  static void exe() throws Exception {
-    //MBG 执行过程中的警告信息
-    var warnings = new ArrayList<String>();
-    //读取我们的 MBG 配置文件
-    var is = GeneratorTest.class.getResourceAsStream("/generatorConfig.xml");
-    var cp = new ConfigurationParser(warnings);
-    var config = cp.parseConfiguration(is);
+    @BeforeAll
+    static void exe() throws Exception {
+        //MBG 执行过程中的警告信息
+        var warnings = new ArrayList<String>();
+        //读取我们的 MBG 配置文件
+        var is = GeneratorTest.class.getResourceAsStream("/generatorConfig.xml");
+        var cp = new ConfigurationParser(warnings);
+        var config = cp.parseConfiguration(is);
 
-    is.close();// 优化
-    //当生成的代码重复时，覆盖原代码
-    var callback = new DefaultShellCallback(true);
-    //创建 MBG
-    var myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-    //执行生成代码
-    myBatisGenerator.generate(null);
-    //输出警告信息
-    for (String warning : warnings) {
-      System.out.println(warning);
+        is.close();// 优化
+        //当生成的代码重复时，覆盖原代码
+        var callback = new DefaultShellCallback(true);
+        //创建 MBG
+        var myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        //执行生成代码
+        myBatisGenerator.generate(null);
+        //输出警告信息
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
     }
-  }
 
-  @Test
-  void basics() throws IOException {
-    InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
-    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-    try (SqlSession session = sqlSessionFactory.openSession()) {
-      var mapper = session.getMapper(UserInfoMapper.class);
-      var user = mapper.selectByPrimaryKey(1L);
-      Assertions.assertTrue(user.isPresent());
+    @Test
+    @SuppressWarnings("unchecked")
+    void basics() throws IOException, ClassNotFoundException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        Long id = 1L;
+        Class<BaseMapper<BaseEntity>> clazz = (Class<BaseMapper<BaseEntity>>) Class.forName("test.dao.UserInfoMapper");
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            var mapper = session.getMapper(clazz);
+            var entity = mapper.selectByPrimaryKey(id);
+            entity.ifPresentOrElse(o -> {
+                Assertions.assertNotNull(o.getGmtCreate());
+                Assertions.assertNotNull(o.getGmtModified());
+                Assertions.assertNotNull(o.getId());
+                Assertions.assertSame(o.getId(), id);
+            }, Assertions::fail);
+        }
     }
-  }
 }
